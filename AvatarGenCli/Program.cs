a8b114c9e99fd1cd4b16558e6203fd2f0b6dbf7d
cainy-a <cainatkinson98@gmail.com>
2020-07-09 15:28:02 +0100

    f

---
+++ AvatarGenCli/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using static AvatarGenLib.AvatarGenerator;
+
+namespace AvatarGenCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Any(x => x.Contains("--help")))
+            {
+                Console.WriteLine(
+@"-- Avatar Generator CLI Help --
+Usage: AvatarGenCli <width> <height> <path to save SVG> <(optional) foreground Red> <(opt) fg Green> <(opt) fg Blue> <(optional) background Red> <(opt) bg Green> <(opt) bg Blue>
+
+Width & Height are NOT PIXELS, they should be somewhere around 4-16. Multiples of 4 look the nicest IMO.
+
+If you don't have write access to the save path, this will fail.
+
+Colours are optional, we have our own defaults.
+
+Made by Cain Atkinson");
+                return;
+            }
+
+            if (args.Length == 0)
+            {
+                Interactive();
+                return;
+            }
+
+            StartGenerator(args);
+        }
+
+        static void StartGenerator(string[] args)
+        {
+            // warning, bad code ahead
+
+        }
+
+        static void Interactive()
+        {
+
+        }
+    }
+}
---
+++ AvatarGenLib/AvatarGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+
+namespace AvatarGenLib
+{
+    public class AvatarGenerator
+    {
+        public static void GenerateSvg(string pathToSave, int width = 4, int? height = null, Color? foregroundColour = null, Color? backgroundColour = null)
+        {
+            // Set Stuff, need to use Nullable<T> because default parameters must be compile-time consistent.
+            if (!foregroundColour.HasValue) foregroundColour = Color.FromArgb(194, 241, 255);
+            if (!backgroundColour.HasValue) backgroundColour = Color.FromArgb(109, 180, 213);
+            if (!height.HasValue) height = width;
+
+            throw new NotImplementedException("I don't know how to do SVGs :(");
+        }
+    }
+}
